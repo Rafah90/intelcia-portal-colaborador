0e3f0a3cb1bd5c1044106cd9c8602d7bc926c33c
Rafah90 <rafael.danjos@hotmail.com>
2025-10-22 11:46:36 +0100

    Update Menu_Inicial.py

--- Menu_Inicial.py
+++ Menu_Inicial.py
@@ -1,2 +1,24 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Menu Inicial",
+    page_icon="📋",
+    layout="centered",  # deixa de ser "wide"
+    initial_sidebar_state="expanded"
+)
+
+# TÍTULO
+st.markdown("<h1>Portal do Colaborador</h1>",unsafe_allow_html=True)
+
+st.markdown("Portal para compartilhamento de informações e solicitações diversas.",unsafe_allow_html=True)
+
+st.markdown("""
+### Aqui você pode:
+
+- 📰 **NOTÍCIAS:** Compartilhamento de comunicados de procedimentos e reminders.  
+
+
+Nos próximos dias novas funcionalidades serão disponibilizadas
+""")
+
+
